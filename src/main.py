b390d11967b5d9b58c4eec73613d8226f55f8835
Breno <brenluz@outlook.com>
2025-01-13 12:01:58 -0300

    O codigo funciona ao ser rodado no local, tentando ajustar para o codigo do servidor

--- src/main.py
+++ src/main.py
@@ -13,10 +13,13 @@ load_dotenv()
 
 
 def main():
-    urlSDO = os.getenv("teste")
-    sdo = getSheet(urlSDO)
+    # pega a data atual
     data = datetime.now()
     ano, mes, dia = str(data).split(" ")[0].split("-")
+    diaAtual = int(dia)
+    # procura o url da planilha
+    urlSDO = os.getenv("teste")
+    sdo = getSheet(urlSDO)
 
     try:
         planilhaMes: Worksheet = sdo.worksheet(sheets.meses[int(mes)])
@@ -25,30 +28,19 @@ def main():
         print(e)
         exit(1)
 
-    diaAtual = int(dia)
+    #  Define a area da planilha que serao inseridas as informacoes
     area = sheets.findArea(planilhaMes, diaAtual)
     colunaInicio = sheets.get_column_for_day(diaAtual)
     colunaEnd = chr(ord(colunaInicio) + 4)
     linhaDia = int(area.split(":")[0][1:])
-    textoDia = "SDO - PUNIÇÕES EM ABERTO NO PODIO NO DIA " + str(diaAtual) + "/" + mes
-    if planilhaMes.find(textoDia) is None:
-        sheets.addInitialText(planilhaMes, diaAtual, mes, area, colunaInicio, colunaEnd, linhaDia + 1, textoDia)
-        linhaDia = linhaDia + 2  # Pula as duas linhas do texto inicial
-    else:
-        linhaDia = linhaDia - 3  # Se o texto inicial ja existe a linhaDia e a primeira linha vazia
-        print("Texto inicial encontrado")
 
-    linhaId = 50
-    if planilhaMes.find('Tarefas adicionadas') is None:
-        planilhaMes.update_acell('A' + str(linhaId), 'Tarefas adicionadas')
-    else:
-        planilhaMes.col_values(1)
-        linhaId = len(planilhaMes.col_values(1))
-    linhaId += 1
+    # Adiciona o texto inicial na planilha
+    linhaDia = sheets.addInitialText(planilhaMes, diaAtual, mes, area, colunaInicio, colunaEnd, linhaDia + 1)
+    linhaDia += 2
 
+    # Pega as tarefas do podio
     sumario_tarefas = podio.get_tasks_in_space()
     tarefas = []
-
     for task in sumario_tarefas['overdue']['tasks']:
         nome_tarefa = task['text']
         task_id = task['task_id']
@@ -58,45 +50,55 @@ def main():
         vitima = task['responsible']['name']
         tarefas.append(Tarefa(task_id, tarefa_data, vitima, nome_tarefa, "Atenção"))
 
-    planilhaMes.get('A' + str(linhaId) + ':A' + str(linhaId + 50))
+    # Pega os ids das tarefas ja adicionadas
+    linhaId = sheets.initialIdAmount(planilhaMes)
     ids = planilhaMes.get('A' + str(50) + ':A')
 
     addedTarefas = 1
-    for tarefa in tarefas:
-        contentUpdate = tarefa.returnAtrributes()
-        contentUpdate.insert(0, addedTarefas)
 
+    for tarefa in tarefas:
         taskNotAdded = True
         for i in ids:
             if i[0] == str(tarefa.taskId):
                 taskNotAdded = False
+                print('task already added')
                 break
 
         if taskNotAdded:
-            if tarefa.data == f"{diaAtual}/{mes}" or tarefa.data == f"{diaAtual - 1}/{mes}":
+            stringDia = sheets.add_leading_zero(str(diaAtual))
+            if tarefa.data == f"{stringDia}/{mes}":
                 print('date is right')
-                taskLinha = int(linhaDia) + addedTarefas + 1
-                cell_num = planilhaMes.cell(taskLinha, ord(colunaInicio) - ord('A') + 1)
-                if not cell_num.value or cell_num.value == '0':
-                    planilhaMes.format(f"{colunaInicio}{taskLinha}" + ":" + f"{colunaEnd}{taskLinha}", {
-                        "borders": {
-                            "top": {"style": "SOLID"},
-                            "bottom": {"style": "SOLID"},
-                            "left": {"style": "SOLID"},
-                            "right": {"style": "SOLID"}
-                        },
-                        "textFormat": {
-                            "foregroundColor": {"blue": 0.0, "green": 0.0, "red": 0.0}},
-                        "horizontalAlignment": "CENTER",
-                        "verticalAlignment": "MIDDLE"
-                    })
-                    for i, col in enumerate(range(ord(colunaInicio), ord(colunaEnd) + 1)):
-                        coluna = chr(col)
-                        print('adding task')
-                        planilhaMes.update_acell(f"{coluna}{taskLinha}", contentUpdate[i])
-                    addedTarefas += 1
-                else:
-                    addedTarefas += 1
+                tarefaNotchecked = True
+
+                contentUpdate = tarefa.returnAtrributes()
+                contentUpdate.insert(0, addedTarefas)
+
+                while tarefaNotchecked:
+                    taskLinha = int(linhaDia) + addedTarefas - 2
+                    contentUpdate[0] = addedTarefas
+                    cell_num = planilhaMes.cell(taskLinha, ord(colunaInicio) - ord('A') + 1)
+                    if not cell_num.value or cell_num.value == '0':
+                        planilhaMes.format(f"{colunaInicio}{taskLinha}" + ":" + f"{colunaEnd}{taskLinha}", {
+                            "borders": {
+                                "top": {"style": "SOLID"},
+                                "bottom": {"style": "SOLID"},
+                                "left": {"style": "SOLID"},
+                                "right": {"style": "SOLID"}
+                            },
+                            "textFormat": {
+                                "foregroundColor": {"blue": 0.0, "green": 0.0, "red": 0.0}},
+                            "horizontalAlignment": "CENTER",
+                            "verticalAlignment": "MIDDLE"
+                        })
+                        planilhaMes.update_acell('A' + str(linhaId), tarefa.taskId)
+                        for i, col in enumerate(range(ord(colunaInicio), ord(colunaEnd) + 1)):
+                            coluna = chr(col)
+                            planilhaMes.update_acell(f"{coluna}{taskLinha}", contentUpdate[i])
+                        addedTarefas += 1
+                        linhaId += 1
+                        tarefaNotchecked = False
+                    else:
+                        addedTarefas += 1
 
 
 main()
--- src/sheets.py
+++ src/sheets.py
@@ -1,4 +1,4 @@
-from gspread import Worksheet
+from gspread import Worksheet, worksheet
 
 meses = {
     1: "Janeiro",
@@ -15,14 +15,13 @@ meses = {
     12: "Dezembro"
 }
 
-
-def findEmpty(worksheet, column):
-    col_values = worksheet.col_values(column)
-    for idx, value in enumerate(col_values, start=1):
-        if value == '':
-            return idx
-    return len(col_values) + 2
-
+column_map = {
+    range(1, 8): 'B',
+    range(8, 15): 'H',
+    range(15, 22): 'N',
+    range(22, 29): 'T',
+    range(29, 32): 'Z'
+}
 
 cell_format = {
     "backgroundColor": {
@@ -57,29 +56,37 @@ cell_format = {
 }
 
 
-def find_last_non_empty_row(worksheet, column):
-    column = ord(column) - ord('A') + 1
-    col_values = worksheet.col_values(column)
-    for idx in range(len(col_values) - 1, -1, -1):
-        if col_values[idx] != '':
-            return idx + 3  # Return the next row after the last non-empty cell
-    return 1  # If the column is completely empty, return the first row
+def findEmpty(planilha: Worksheet, column):
+    col_values = planilha.col_values(column)
+    for idx, value in enumerate(col_values, start=1):
+        if value == '':
+            return idx
+    return len(col_values) + 2
 
 
-column_map = {
-    range(1, 8): 'B',
-    range(8, 15): 'H',
-    range(15, 22): 'N',
-    range(22, 29): 'T',
-    range(29, 32): 'Z'
-}
+def find_last_non_empty_row(planilha: Worksheet, column):
+    coluna = ord(column) - ord('A') + 1
+    texto = "SDO - PUNIÇÕES EM ABERTO NO PODIO NO DIA xx/xx"
+
+    if planilha.find(texto, in_column=coluna) is None:
+        col_values = planilha.col_values(coluna)
+        print(col_values)
+        for idx in range(len(col_values) - 1, -1, -1):
+            if col_values[idx] == '0':
+                return idx + 3
+            if col_values[idx] != '':
+                print(idx)
+                return idx + 4  # Return the next row after the last non-empty cell
+    elif planilha.find(texto) is not None:
+        return planilha.find(texto).row
 
 
-def findArea(worksheet, dia):
+def findArea(planilha: Worksheet, dia):
     area = None
     for day_range, col in column_map.items():
         if int(dia) in day_range:
-            first_empty_row = find_last_non_empty_row(worksheet, col)
+            first_empty_row = find_last_non_empty_row(planilha, col)
+            # print(f"First empty row for day {dia} is {first_empty_row}")
             area = f"{col}{first_empty_row}:{chr(ord(col) + 4)}{first_empty_row}"
             break
     return area
@@ -92,18 +99,60 @@ def get_column_for_day(dia):
     raise KeyError(f"Day {dia} is not in any range")
 
 
-def addInitialText(planilha: Worksheet, dia: int, mes: int, area: str, colunaInicio, colunaEnd, linha, textoDia):
-    print("Texto inicial não encontrado, inserindo")
+def add_leading_zero(value: str):
+    return value.zfill(2)
 
-    textoDia = "SDO - PUNIÇÕES EM ABERTO NO PODIO NO DIA " + str(dia) + "/" + str(mes)
-    planilha.merge_cells(area)
-    planilha.format(area, cell_format)
-    planilha.update_acell(area.split(":")[0], textoDia)
 
-    modelo_texto = ["Nº", "DATA DA ENTREGA", "MEMBRO", "ATIVIDADE", "PUNIÇÃO"]
-    planilha.format(f"{colunaInicio}{linha}" + ":" + f"{colunaEnd}{linha}", cell_format)
+def addInitialText(planilha: Worksheet, dia: int, mes: int, area: str, colunaInicio, colunaEnd, linha):
+    diaText = add_leading_zero(str(dia))
+    textoDia = f"SDO - PUNIÇÕES EM ABERTO NO PODIO NO DIA {diaText}/{mes}"
 
-    for i, col in enumerate(range(ord(colunaInicio), ord(colunaEnd) + 1)):
-        texto = modelo_texto[i]
-        planilha.update_acell(f"{chr(col)}{linha}", texto)
-    print("Texto  inicial inserido")
+    if planilha.find(textoDia) is None:
+        print("Texto inicial não encontrado, inserindo")
+        planilha.merge_cells(area)
+        planilha.format(area, cell_format)
+        planilha.update_acell(area.split(":")[0], textoDia)
+
+        modelo_texto = ["Nº", "DATA DA ENTREGA", "MEMBRO", "ATIVIDADE", "PUNIÇÃO"]
+        planilha.format(f"{colunaInicio}{linha}" + ":" + f"{colunaEnd}{linha}", cell_format)
+
+        for i, col in enumerate(range(ord(colunaInicio), ord(colunaEnd) + 1)):
+            texto = modelo_texto[i]
+            planilha.update_acell(f"{chr(col)}{linha}", texto)
+        print("Texto  inicial inserido")
+
+        for i in range(0, 5):
+            planilha.format(f"{chr(ord(colunaInicio) + i)}{linha + 1}", {
+                "borders":
+                    {
+                        "top": {
+                            "style": "SOLID"
+                        },
+                        "bottom": {
+                            "style": "SOLID"
+                        },
+                        "left": {
+                            "style": "SOLID"
+                        },
+                        "right": {
+                            "style": "SOLID"
+                        }
+                    }
+            })
+        return linha
+    else:
+        print("Texto inicial encontrado")
+        celula = planilha.find(textoDia)
+        print('row: ', celula.row)
+        return int(celula.row) + 1
+
+
+# Looks for already existing ids, if none adds initial id text
+def initialIdAmount(planilha: worksheet):
+    linha = 50
+    if planilha.find('Tarefas adicionadas') is None:
+        planilha.update_acell('A' + str(linha), 'Tarefas adicionadas')
+    else:
+        linha = len(planilha.col_values(1))
+    linha += 1
+    return linha
